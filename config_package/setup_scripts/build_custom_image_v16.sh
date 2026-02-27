c773bece39cce839d88706acca36eaeb298c8ef9
shy <601978865@qq.com>
2026-02-28 00:45:00 +0800

    fix: add proxy arguments to docker build to fix apt/npm hang in China

--- config_package/setup_scripts/build_custom_image_v16.sh
+++ config_package/setup_scripts/build_custom_image_v16.sh
@@ -11,8 +11,16 @@ echo "Building custom ERPNext (v16.7.2) + HRMS (version-16) image..."
 # Export apps.json to base64
 APPS_JSON_BASE64=$(base64 -w 0 apps_v16.json)
 
-# Build command
+# Build command (with proxy to speed up npm/apt in China)
+# 请确保你的 192.168.1.12:7890 代理已开启允许局域网连接
+export HTTP_PROXY="http://192.168.1.12:7890"
+export HTTPS_PROXY="http://192.168.1.12:7890"
+
 docker build \
+  --build-arg=HTTP_PROXY=$HTTP_PROXY \
+  --build-arg=HTTPS_PROXY=$HTTPS_PROXY \
+  --build-arg=http_proxy=$HTTP_PROXY \
+  --build-arg=https_proxy=$HTTPS_PROXY \
   --build-arg=FRAPPE_PATH=https://github.com/frappe/frappe \
   --build-arg=FRAPPE_BRANCH=$FRAPPE_BRANCH \
   --build-arg=PYTHON_VERSION=$PYTHON_VERSION \
